383b8374d229d7c798968f3351adf6b809923aec
Atharv0698 <atharvkulkarni0698@gmail.com>
2025-07-15 21:17:14 +0200

    Save final work prior to switch to main

--- client/main.cpp
+++ client/main.cpp
@@ -5,14 +5,15 @@
 #include <netdb.h>
 
 int main() {
-    std::cout << "[CLIENT] Starting periodic UDP sender...\n";
 
+    //initialze socket 
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
-        perror("Failed to create socket");
+        std::cout<<"[CLIENT] Failed to create socket";
         return 1;
     }
-
+    
+    //define address IPV4 for communication, UDP Socket
     struct addrinfo hints{}, *res;
     memset(&hints, 0, sizeof(hints));
     hints.ai_family = AF_INET;
@@ -20,35 +21,37 @@ int main() {
 
     int status = getaddrinfo("server", nullptr, &hints, &res);
     if (status != 0) {
-        std::cerr << "getaddrinfo error: " << gai_strerror(status) << std::endl;
+        std::cout << " [Client] addr info error" << std::endl;
         close(sockfd);
         return 1;
     }
-
+    
+    //define server address
     sockaddr_in servaddr{};
     servaddr.sin_family = AF_INET;
     servaddr.sin_port = htons(8080);
     servaddr.sin_addr = ((sockaddr_in*)res->ai_addr)->sin_addr;
-
+    
+    std::cout << "[Client] Starting periodic UDP sender...\n";
 
     int sequence = 0;
 
     while (true) {
-        std::string message = "SEQ:" + std::to_string(sequence);
-        ssize_t sent = sendto(sockfd, message.c_str(), message.size(), 0,
-                              (const struct sockaddr*)&servaddr, sizeof(servaddr));
+
+        //send message
+        std::string message = "SEQUENCE:" + std::to_string(sequence);
+        size_t sent = sendto(sockfd, message.c_str(), message.size(), 0, (const struct sockaddr*)&servaddr, sizeof(servaddr));
 
         if (sent < 0) {
-            perror("send operation failed");
+            std::cout<<"[CLIENT] send operation failed";
             break;
         }
 
         std::cout << "[CLIENT] Sent message: " << message << std::endl;
-       
-        
+             
         sequence++;
 
-        sleep(100);
+        sleep(100); // Set to 0.01 Hz
 
     }
 
--- server/main.cpp
+++ server/main.cpp
@@ -5,50 +5,65 @@
 #include <string>
 
 int main() {
+
+    //initialize a socket for communication
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
     if (sockfd < 0) {
-        perror("Failed to create socket");
+        std::cout<<"[SERVER] Failed to create socket";
         return 1;
     }
-
+    
+    //create server address structure
     sockaddr_in servaddr{};
     memset(&servaddr, 0, sizeof(servaddr));
+
     servaddr.sin_family = AF_INET;
     servaddr.sin_addr.s_addr = INADDR_ANY;
     servaddr.sin_port = htons(8080);
 
+    //Binding socket to the port
     if (bind(sockfd, (sockaddr*)&servaddr, sizeof(servaddr)) < 0) {
-        perror("bind failed");
+        std::cout<<"[SERVER] bind failed";
         return 1;
     }
 
-    std::cout << "[SERVER] Listening for UDP messages...\n";
+    std::cout << "[SERVER] listening to UDP packets\n";
 
     char buffer[1024];
     sockaddr_in clientaddr{};
     socklen_t len = sizeof(clientaddr);
-
+    
+    // initilize a storing variable
     int last_sequence = -1;
 
     while (true) {
-        ssize_t n = recvfrom(sockfd, buffer, sizeof(buffer) - 1, 0,
-                            (sockaddr*)&clientaddr, &len);
+
+        // receive client messages
+        size_t n = recvfrom(sockfd, buffer, sizeof(buffer) - 1, 0,(sockaddr*)&clientaddr, &len);
+        
         if (n < 0) {
-            perror("recvfrom failed");
+            std::cout<<"[SERVER] recieve from client failed";
             break;
         }
 
-        buffer[n] = '\0';  // Null-terminate buffer
+        buffer[n] = '\0'; 
 
         std::cout << "[SERVER] Received: " << buffer << std::endl;
 
         int received_seq = -1;
-        if (sscanf(buffer, "SEQ:%d", &received_seq) == 1) {
+
+        if (sscanf(buffer, "SEQUENCE:%d", &received_seq) == 1) 
+        {
             if (received_seq > last_sequence) {
-                std::cout << "[SERVER] Sequence valid, Current: " << received_seq << "[SERVER] Sequence valid, last: "<< last_sequence<<std::endl;
+                std::cout << "[SERVER] Sequence valid, Current: " << received_seq << ", Previous: "<< last_sequence<<std::endl;
                 last_sequence = received_seq;
-            } else {
-                std::cout << "[SERVER] Out-of-order or repeated sequence. Last: " << last_sequence << ", Received: " << received_seq << std::endl;
+            } 
+            else if (received_seq == last_sequence) {
+                std::cout << "[SERVER] Repeated sequence. Previous: " << last_sequence << ", Received: " << received_seq << std::endl;
+            }
+            else{
+                std::cout << "[SERVER] Out of Order"<<std::endl;
+                last_sequence = received_seq;
             }
         } else {
             std::cout << "[SERVER] Could not parse sequence number." << std::endl;
