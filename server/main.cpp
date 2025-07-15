bc730f4c15c53d775d5e8f8197ae6e555b3eef67
Atharv0698 <atharvkulkarni0698@gmail.com>
2025-07-15 16:27:50 +0200

    Sequence is sent and acknowleged and validated

--- server/main.cpp
+++ server/main.cpp
@@ -2,6 +2,7 @@
 #include <arpa/inet.h>
 #include <unistd.h>
 #include <cstring>
+#include <string>
 
 int main() {
     int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
@@ -21,23 +22,37 @@ int main() {
         return 1;
     }
 
-    std::cout << "Started server, waiting for messages...\n";
+    std::cout << "[SERVER] Listening for UDP messages...\n";
 
     char buffer[1024];
     sockaddr_in clientaddr{};
     socklen_t len = sizeof(clientaddr);
 
+    int last_sequence = -1;
+
     while (true) {
         ssize_t n = recvfrom(sockfd, buffer, sizeof(buffer) - 1, 0,
-                             (sockaddr*)&clientaddr, &len);
+                            (sockaddr*)&clientaddr, &len);
         if (n < 0) {
             perror("recvfrom failed");
             break;
         }
 
-        buffer[n] = '\0';
-        std::cout << "Server received: " << buffer << std::endl;
+        buffer[n] = '\0';  // Null-terminate buffer
 
+        std::cout << "[SERVER] Received: " << buffer << std::endl;
+
+        int received_seq = -1;
+        if (sscanf(buffer, "SEQ:%d", &received_seq) == 1) {
+            if (received_seq > last_sequence) {
+                std::cout << "[SERVER] Sequence valid, Current: " << received_seq << "[SERVER] Sequence valid, last: "<< last_sequence<<std::endl;
+                last_sequence = received_seq;
+            } else {
+                std::cout << "[SERVER] Out-of-order or repeated sequence. Last: " << last_sequence << ", Received: " << received_seq << std::endl;
+            }
+        } else {
+            std::cout << "[SERVER] Could not parse sequence number." << std::endl;
+        }
     }
 
     close(sockfd);
